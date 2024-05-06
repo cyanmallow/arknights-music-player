0a97f1f6fc53e54f6a744a123156d5138336d5a9
cyanmallow <ggmmallow@gmail.com>
2024-05-06 23:15:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const allSongs = [
+    {
+        id: 1,
+        title: "Operation Pyrite",
+        artist: "MSR",
+        duration: "4:22",
+        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+    },
+    {
+        id: 2,
+        title: "Operation Blade",
+        artist: "MSR",
+        duration: "4:22",
+        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+    },    
+    {
+        id: 3,
+        title: "Operation Cinder",
+        artist: "MSR",
+        duration: "4:22",
+        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+    },    
+    {
+        id: 4,
+        title: "Operation Lead Seal",
+        artist: "MSR",
+        duration: "4:22",
+        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+    },    
+    {
+        id: 5,
+        title: "Operation Spectrum",
+        artist: "MSR",
+        duration: "4:22",
+        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+    },    
+    {
+        id: 6,
+        title: "Operation Wild Scales",
+        artist: "MSR",
+        duration: "4:22",
+        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+    },
+]
