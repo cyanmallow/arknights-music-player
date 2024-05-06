ea9664451a477b5fddb66d9f33cb83927e14dd01
cyanmallow <ggmmallow@gmail.com>
2024-05-07 01:13:51 +0700

    this can play music now

--- script.js
+++ script.js
@@ -4,41 +4,112 @@ const allSongs = [
         title: "Operation Pyrite",
         artist: "MSR",
         duration: "4:22",
-        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+        src: "cc_src/Pyrite.mp3",
     },
     {
         id: 2,
         title: "Operation Blade",
         artist: "MSR",
-        duration: "4:22",
-        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+        duration: "3:58",
+        src: "cc_src/Blade.mp3",
     },    
     {
         id: 3,
         title: "Operation Cinder",
         artist: "MSR",
-        duration: "4:22",
-        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+        duration: "4:13",
+        src: "cc_src/Cinder.mp3",
     },    
     {
         id: 4,
         title: "Operation Lead Seal",
         artist: "MSR",
-        duration: "4:22",
-        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+        duration: "3:40",
+        src: "cc_src/Lead_Seal.mp3",
     },    
     {
         id: 5,
         title: "Operation Spectrum",
         artist: "MSR",
-        duration: "4:22",
-        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+        duration: "4:27",
+        src: "cc_src/Spectrum.mp3",
     },    
     {
         id: 6,
         title: "Operation Wild Scales",
         artist: "MSR",
-        duration: "4:22",
-        src: "https://open.spotify.com/track/3ghYly2jUXbPlOJ7omJtP8?si=0beb83dcdc944cff",
+        duration: "4:04",
+        src: "cc_src/Wild_Scales.mp3",
     },
-]
+];
+
+// set up audio and userdata
+const audio = new Audio();
+let userData = {
+    songs: [...allSongs],
+    currentSong: null,
+    songCurrentTime: 0,
+};
+
+// show playlist 
+const renderSongs = (array) => {
+    const songsHTML = array.map((song) => {
+        return `
+        <li id="song-${song.id}" class ="playlist-song">
+        <button class="song-info onclick="playSong(${song.id})">
+            <span class="song-title">${song.title}</span>
+            <span class="song-artist">${song.artist}</span>
+            <span class="song-duration">${song.duration}</span>
+        </button>
+        `;
+    }).join('');
+
+    // the li tag at the end of the HTML file
+    const playlistSongs = document.getElementById('playlist-songs');
+    // change the HTML content of it to songsHTML
+    playlistSongs.innerHTML = songsHTML;
+};
+
+
+// play button
+const playButton = document.getElementById("play");
+const playSong = (id) => {
+    const song = userData?.songs.find((song) => song.id === id);
+    audio.src = song.src;
+    audio.title = song.title;
+
+    // check if there are any songs playing
+    if (userData?.currentSong === null){
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData?.songCurrentTime;
+    }
+
+    // after user press play, update currentSong
+    userData.currentSong = song; 
+    playButton.classList.add("playing");
+    // and start the audio
+    audio.play();
+}
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong == null){
+        playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+})
+
+const pauseSong = () => {
+    //TODO
+};
+
+const playNextSong = () => {
+
+};
+
+const playPreviousSong = () => {
+
+};
+
+renderSongs(userData?.songs);
